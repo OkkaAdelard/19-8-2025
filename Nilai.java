c4b4a47a1c93a9b4bf154cda51861f759f4fa7dc
OkkaAdelard <okka_syahbarr_34rpl@student.smktelkom-mlg.sch.id>
2025-08-25 11:04:42 +0700

    Add files via upload

--- Nilai.java
+++ Nilai.java
@@ -6,31 +6,30 @@ public class Nilai {
         System.out.println("Masukkan nilai Anda:");
         int nilai = input.nextInt();
         
-        if (nilai >= 95) {
+        if (nilai >= 95 && nilai <= 100) { 
             System.out.println("Nilai Anda A+");
-        } else if (nilai >= 90) {
+        } else if (nilai >= 90 && nilai < 95 ) {
             System.out.println("Nilai Anda A");
-        } else if (nilai >= 85) {
+        } else if (nilai >= 85 && nilai < 90) {
             System.out.println("Nilai Anda A-");
         } else if
-        (nilai >= 80) {
+        (nilai >= 80 && nilai < 85) {
             System.out.println("Nilai Anda B");
-        } else if (nilai >= 70) {
+        } else if (nilai >= 70 && nilai < 80) {
             System.out.println("Nilai Anda C");
-        } else if (nilai >= 60) {
+        } else if (nilai >= 60 && nilai < 70) {
             System.out.println("Nilai Anda D");
-        } else {
+         } else if (nilai < 60 && nilai >= 0) {
             System.out.println("Nilai Anda E");
-        }
-        
-        System.out.println("============================");
+        } else {
+            System.out.println("================================");
+            System.out.println("Nilai tidak valid. Masukkan nilai antara 0-100.");
 
-        input.close();
-        if (nilai < 75) {
-            System.out.println("Anda harus mengulang ujian.");
-        } 
-        else {
-            System.out.println("Selamat, Anda lulus ujian!");
+        if (nilai >= 75 && nilai <= 100) {
+            System.out.println("Selamat, Anda lulus!");
+        } else if (nilai < 75 && nilai >= 0) {
+            System.out.println("Maaf, Anda tidak lulus.");
+        }
     }
 }
 }
